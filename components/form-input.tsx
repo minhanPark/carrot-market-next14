25e9555dce2d0865e6ab0aabae455617478d25a2
minhanPark <alsgks1218@naver.com>
2024-05-08 13:17:23 +0900

    input의 원래 속성도 사용하기 위해 기존 props에 input의 props로 확장

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -1,26 +1,21 @@
+import { InputHTMLAttributes } from "react";
+
 type FormInput = {
-  type: string;
-  placeholder: string;
-  required: boolean;
-  errors?: string[];
   name: string;
+  errors?: string[];
 };
 
 export default function FormInput({
-  type,
-  placeholder,
-  required,
   errors = [],
   name,
-}: FormInput) {
+  ...rest
+}: FormInput & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className=" flex flex-col gap-2">
       <input
-        type={type}
         name={name}
-        placeholder={placeholder}
-        required={required}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        {...rest}
       />
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
