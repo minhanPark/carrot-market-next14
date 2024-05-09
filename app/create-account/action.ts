a0aa201acfc12f78ef5bf03fabd9598fa7e84a2c
minhanPark <alsgks1218@naver.com>
2024-05-09 15:54:27 +0900

    bcrypt 다운 받고 유저 가입 테스트

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import db from "@/libs/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkPasswords = ({
   password,
@@ -10,10 +12,47 @@ const checkPasswords = ({
   passwordConfirmation: string;
 }) => password === passwordConfirmation;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
-    username: z.string().trim().min(3, "short!").max(10),
-    email: z.string().email(),
+    username: z
+      .string()
+      .trim()
+      .min(3, "short!")
+      .max(10)
+      .refine(checkUniqueUsername, "This username is already taken"),
+    email: z
+      .string()
+      .email()
+      .refine(checkUniqueEmail, "This email is already taken"),
     password: z.string().min(10),
     passwordConfirmation: z.string().min(10),
   })
@@ -29,10 +68,22 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     passwordConfirmation: formData.get("passwordConfirmation"),
   };
-  const result = formSchema.safeParse(data);
+
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
   }
 }
