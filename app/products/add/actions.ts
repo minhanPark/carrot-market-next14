d27ab43dd79034948bc51c5fccb82de43c9a9270
minhanPark <alsgks1218@naver.com>
2024-05-17 15:04:34 +0900

    image add action 코드 수정

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,5 +1,25 @@
 "use server";
 
+import db from "@/libs/db";
+import getSession from "@/libs/session";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+});
+
 export async function uploadProduct(formData: FormData) {
   const data = {
     photo: formData.get("photo"),
@@ -7,5 +27,33 @@ export async function uploadProduct(formData: FormData) {
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    console.log(photoData);
+  }
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: "",
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
